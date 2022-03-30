3404ff4afe7705cc934f066135f30a24d8fd01e2
PyAver <78295747+PyAver@users.noreply.github.com>
2022-03-30 22:14:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 if ($request.headers['Cookie']) {
-  const cookie = $request.headers['Cookie'];
-  const url = $request.url;
-  const walkr_auth_token = url.split('auth_token=')[1].split('&')[0];
+	const cookie = $request.headers['Cookie'];
+	const url = $request.url;
+	const walkr_auth_token = url.split('auth_token=')[1].split('&')[0];
 
-  const saveCookie = $persistentStore.write(cookie, "WalkrCookie");
-  const saveToken = $persistentStore.write(walkr_auth_token, "WalkrToken");
+	console.log('url: ' + url);
+	console.log('cookie: ' + cookie);
+	console.log('walkr_auth_token: ' + walkr_auth_token);
 
-  if (!(saveCookie && saveToken)) {
+	const saveCookie = $persistentStore.write(cookie, "WalkrCookie");
+	const saveToken = $persistentStore.write(walkr_auth_token, "WalkrToken");
+
+	if (!(saveCookie && saveToken)) {
         $notification.post("Walkr Cookie 保存錯誤‼️", "", "請重新登入")
     } else {
         $notification.post("Walkr Cookie 保存成功🎉", "", "")
